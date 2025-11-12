5f3bdd97f7d39cfc65de144ba43ab393615b3654
Xin1306 <23005011@siswa.um.edu.my>
2025-11-12 21:54:59 +0900

    latest

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -3,8 +3,6 @@ const router = express.Router();
 const authController = require('../controllers/authController');
 
 //GET
-router.get('/SignUp', authController.getSignUpPage);
-router.get('/Login', authController.getLoginPage);
 router.get('/Logout', authController.logoutUser);
 
 //POST 
